b2f43a8a05e2f24d5c4c774deaf09e07b1b4bb49
egbertn <egbert_nierop@outlook.com>
2019-04-27 08:59:53 +0200

    content added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ispsession.io.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var ctr = (int)(Session["counter"] ?? 0);
+            ctr++;
+            Session["counter"] = ctr;
+            var application = (IApplicationCache)this.HttpContext.Items["_ISPApplicationCache"];
+
+
+            var someViewClass = new SomeViewClass
+            {
+                SomeString = (string)((object[])application["cachethis"])[0],
+                someDate = (DateTime)((object[])application["cachethis"])[1]
+            };
+            return View(someViewClass);
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using ispsession.io.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication5
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public override void Init()
+        {
+            this.PostAcquireRequestState += MvcApplication_PostAcquireRequestState;
+            base.Init();
+        }
+
+        private void MvcApplication_PostAcquireRequestState(object sender, EventArgs e)
+        {
+            var application = (IApplicationCache)HttpContext.Current.Items["_ISPApplicationCache"];
+
+            application["cachethis"] = new object[] { "Highly Cachable stuff", DateTime.Now };
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
